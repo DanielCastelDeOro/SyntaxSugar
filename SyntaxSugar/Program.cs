7892837e6470eb173153f800748893d08c69b4fc
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-09 04:25:14 -0600

    Add project files.

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anwser = 4;
+            string response;
+
+            if (anwser < 9)
+            {
+                response = anwser + "is less than 9";
+            }
+            else
+            {
+                response = anwser + "greater than or equal to 9";
+            }
+            {// im not sure why the response is underlined in red??
+                var response = (anwser < 9) ? $"{anwser} is less than 9" : $"{anwser} is greater than or equal to 9";
+            }
+            {//but a cap "R" is good???
+                var Response = (anwser < 9) ? $"{anwser} is less than 9" : $"{anwser} is greater than or equal to 9";
+            }
+            
+
+            
+            
+            
+
+
+
+
+        }
+    }
+}
